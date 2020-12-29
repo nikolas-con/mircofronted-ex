5b014f521a4faf95c097581d1fc915d817c8281c
nikolas-con <nikos.a.constantinou@gmail.com>
2020-12-29 12:08:07 +0200

    axsas

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { StylesProvider } from "@material-ui/core/styles";
-// import Landing from "./components/Landing";
+import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
 const App = () => {
