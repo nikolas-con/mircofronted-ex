0fbdcecae6aaec392ce559249c26ae2f00a9490e
nikolas-con <nikos.a.constantinou@gmail.com>
2020-12-30 10:18:22 +0200

    set up the navigation bettow on container and marketing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,19 @@
 import React, { useRef, useEffect } from "react";
 import { mount } from "marketing/MarketingApp";
+import { useHistory } from "react-router-dom";
 
 const MarketingApp = () => {
   const ref = useRef(null);
+  const history = useHistory();
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigations: ({ pathname: nextPathName }) => {
+        const { pathname } = history.location;
+        pathname !== nextPathName && history.push(nextPathName);
+      },
+    });
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createMemoryHistory } from "history";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-const mount = (el) => {
-  const history = createMemoryHistory();
+const mount = (el, { onNavigations, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  onNavigations && history.listen(onNavigations);
+
   ReactDOM.render(<App history={history} />, el);
+  return {
+    onParentNavigate: ({ pathname: nextPathName }) => {
+      const { pathname } = history.location;
+      nextPathName !== pathname && history.push(nextPathName);
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.getElementById("_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 export { mount };
